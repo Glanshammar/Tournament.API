8602fa311fd81e53589b39482601af9c817be879
Andreas Johansson <glanshammar70@gmail.com>
2024-12-03 20:11:02 +0100

    Added extension

---
+++ Tournament.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Tournament.Data.Data;
+
+namespace Tournament.API.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task SeedDataAsync(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<TournamentAPIContext>();
+            await SeedData.InitializeAsync(context);
+        }
+    }
+}
