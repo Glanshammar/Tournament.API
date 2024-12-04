565235e9b33c2e21e832ffe18f05be9dfb3224cf
Andreas Johansson <glanshammar70@gmail.com>
2024-12-04 06:29:43 +0100

    Added DTOs

---
+++ Tournament.Core/Dto/GameDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Dto
+{
+    public class GameDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters.")]
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
---
+++ Tournament.Core/Dto/TournamentDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tournament.Core.Dto
+{
+    public class TournamentDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters.")]
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate => StartDate.AddMonths(3);
+    }
+}
