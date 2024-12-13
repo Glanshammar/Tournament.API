ad03df62c945e7a2cb800b25e22e5c543034e9e4
Andreas Johansson <glanshammar70@gmail.com>
2024-12-13 15:00:26 +0100

    Added Service.Contracts

---
+++ Service.Contracts/Interfaces/IGameService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Dto;
+
+namespace Service.Contracts.Interfaces
+{
+    public interface IGameService
+    {
+        Task<IEnumerable<GameDto>> GetAllGamesAsync(int pageNumber, int pageSize);
+        Task<GameDto> GetGameByIdAsync(int id);
+        Task<IEnumerable<GameDto>> GetGamesByTitleAsync(string title, int pageNumber, int pageSize);
+        Task<GameDto> CreateGameAsync(GameDto gameDto);
+        Task UpdateGameAsync(int id, GameDto gameDto);
+        Task DeleteGameAsync(int id);
+    }
+}
---
+++ Service.Contracts/Interfaces/ITournamentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Dto;
+
+namespace Service.Contracts.Interfaces
+{
+    public interface ITournamentService
+    {
+        Task<IEnumerable<TournamentDto>> GetAllTournamentsAsync(int pageNumber, int pageSize);
+        Task<TournamentDto> GetTournamentByIdAsync(int id);
+        Task<TournamentDto> CreateTournamentAsync(TournamentDto tournamentDto);
+        Task UpdateTournamentAsync(int id, TournamentDto tournamentDto);
+        Task DeleteTournamentAsync(int id);
+    }
+}
