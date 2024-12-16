17e59c63a46afa212ee23272b7d987538631b7c7
Andreas Johansson <glanshammar70@gmail.com>
2024-12-16 17:31:09 +0100

    Added mapping profile

---
+++ Tournament.Data/Data/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Tournament.Core.Entities;
+using Tournament.Core.Dto;
+
+namespace Tournament.Data.Data
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Map TournamentDetails to TournamentDto and vice versa
+            CreateMap<TournamentDetails, TournamentDto>().ReverseMap();
+
+            // Map Game to GameDto and vice versa
+            CreateMap<Game, GameDto>().ReverseMap();
+        }
+    }
+}
