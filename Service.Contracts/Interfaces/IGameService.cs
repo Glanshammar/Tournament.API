67fda4de3c21bbe86f9477fac3fed83af703f278
Andreas Johansson <glanshammar70@gmail.com>
2024-12-16 16:58:43 +0100

    Updated interfaces

--- Service.Contracts/Interfaces/IGameService.cs
+++ Service.Contracts/Interfaces/IGameService.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tournament.Core.Dto;
+using Tournament.Core.Utilities;
 
 namespace Service.Contracts.Interfaces
 {
     public interface IGameService
     {
-        Task<IEnumerable<GameDto>> GetAllGamesAsync(int pageNumber, int pageSize);
+        Task<PagedResponse<GameDto>> GetAllGamesAsync(int pageNumber, int pageSize);
         Task<GameDto> GetGameByIdAsync(int id);
-        Task<IEnumerable<GameDto>> GetGamesByTitleAsync(string title, int pageNumber, int pageSize);
+        Task<PagedResponse<GameDto>> GetGamesByTitleAsync(string title, int pageNumber, int pageSize);
         Task<GameDto> CreateGameAsync(GameDto gameDto);
+        Task<GameDto> PatchGameAsync(int gameId, JsonPatchDocument<GameDto> patchDocument, ModelStateDictionary modelState);
+        Task<PagedResponse<GameDto>> GetGamesAsync(string title, int pageNumber, int pageSize);
         Task UpdateGameAsync(int id, GameDto gameDto);
         Task DeleteGameAsync(int id);
     }
--- Service.Contracts/Interfaces/ITournamentService.cs
+++ Service.Contracts/Interfaces/ITournamentService.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tournament.Core.Dto;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Tournament.Core.Entities;
+using Tournament.Core.Utilities;
 
 namespace Service.Contracts.Interfaces
 {
     public interface ITournamentService
     {
-        Task<IEnumerable<TournamentDto>> GetAllTournamentsAsync(int pageNumber, int pageSize);
+        Task<PagedResponse<TournamentDto>> GetAllTournamentsAsync(int pageNumber, int pageSize);
         Task<TournamentDto> GetTournamentByIdAsync(int id);
         Task<TournamentDto> CreateTournamentAsync(TournamentDto tournamentDto);
+        Task<TournamentDto> PatchTournamentAsync(int id, JsonPatchDocument<TournamentDto> patchDocument, ModelStateDictionary modelState);
         Task UpdateTournamentAsync(int id, TournamentDto tournamentDto);
         Task DeleteTournamentAsync(int id);
     }
